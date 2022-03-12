41595ecdd3cdf9a99462517f3fa6f04b12b9a6fe
axexap <101452798+axexap@users.noreply.github.com>
2022-03-12 12:26:53 +0300

    Create quest.js
    message

---
+++ quest.js
@@ -0,0 +1,12 @@
+void xxx()
+       {
+           printf("Hello world\n\n\n");
+           return;
+       }
+ 
+       int main()
+       {
+        
+           xxx();
+           return 0;
+       }
